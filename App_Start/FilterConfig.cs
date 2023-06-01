4e355e1c9123b5d3fa9cb884a871685cf0aaaf2a
Krishna Kumar <kumarkrishna52088@gmail.com>
2023-05-31 23:22:29 -0700

    first commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitLearninDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitLearninDemo.MvcApplication" Language="C#" %>
